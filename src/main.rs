456f3a9cf1ed724d9a35cc461216caa2ddb58494
chahat-101 <chahat-101@users.noreply.github.com>
2026-01-15 23:22:33 +0530

    Version 0.0

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,102 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use colored::*;
 mod utils;
-use utils::{BlockSample, block_color, calculate_block_size};
-
+use image::RgbImage;
 #[derive(Parser)]
 struct Args {
     #[arg(long, alias = "p")]
     path: String,
+
+    #[arg(long, value_enum, default_value_t = CharSet::Default)]
+    charset: CharSet,
     #[arg(long, default_value_t = 100)]
     width: u32,
 }
 
+#[derive(Clone, ValueEnum)]
 enum CharSet {
     Retro,
     Default,
     Light,
+    Detailed,
+}
+
+fn get_charset(charset: CharSet) -> Vec<char> {
+    let chars_string = match charset {
+        // Blocks (Good for solid UIs)
+        CharSet::Retro => " ░▒▓█",
+
+        // Standard ASCII (Good balance)
+        CharSet::Default => "@%#*+=-:. ",
+
+        // Minimal (High contrast)
+        CharSet::Light => "#+-. ",
+
+        // High Detail (Smooth gradients for high res)
+        CharSet::Detailed => {
+            "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+        }
+    };
+
+    chars_string.chars().collect()
+}
+
+const WEIGHTS: [f32; 3] = [0.299, 0.587, 0.114];
+struct BlockSample {
+    lum: f32,
+    r: u8,
+    b: u8,
+    g: u8,
+}
+
+fn block_color(img: &RgbImage, x0: u32, y0: u32, w: u32, h: u32) -> BlockSample {
+    let (img_w, img_h) = img.dimensions();
+
+    let mut r_sum = 0u32;
+    let mut g_sum = 0u32;
+    let mut b_sum = 0u32;
+    let mut count = 0u32;
+
+    let y_end = (y0 + h).min(img_h);
+    let x_end = (x0 + w).min(img_w);
+
+    for y in y0..y_end {
+        for x in x0..x_end {
+            let pixel = img.get_pixel(x, y);
+
+            r_sum += pixel[0] as u32;
+            g_sum += pixel[1] as u32;
+            b_sum += pixel[2] as u32;
+            count += 1;
+        }
+    }
+    let r = (r_sum / count) as u8;
+    let g = (g_sum / count) as u8;
+    let b = (b_sum / count) as u8;
+
+    let brightness = WEIGHTS[0] * r as f32 + WEIGHTS[1] * g as f32 + WEIGHTS[2] * b as f32;
+
+    BlockSample {
+        lum: brightness,
+        r,
+        g,
+        b,
+    }
+}
+
+fn calculate_block_size(
+    img_width: u32,
+    img_height: u32,
+    target_width: u32,
+    aspect: f32,
+) -> (u32, u32) {
+    let block_w = img_width as f32 / target_width as f32;
+    let block_h = block_w as f32 / aspect;
+
+    let block_w = block_w.max(1.0).round() as u32;
+    let block_h = block_h.max(1.0).round() as u32;
+
+    (block_w, block_h)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,8 +104,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = args.path;
     let target_width = args.width;
 
-    //let ascii_chars: Vec<char> = "Ña#W$9876543210?!abc;:+=-,._".chars().collect();
-    let ascii_chars: Vec<char> = " ░▒▓█".chars().collect();
+    let ascii_chars = get_charset(args.charset);
     let img = image::open(path)?.to_rgb8();
     let (img_width, img_height) = img.dimensions();
     let aspect = 0.5;
